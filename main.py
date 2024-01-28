8f75d86fbdd335e5b414b0f4e2dc7c405e32c932
Radovan Semjon <radovan.semjon@gmail.com>
2024-01-28 12:11:11 +0100

    final changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from scraper import Scraper
 def main()->None:
     scraper = Scraper()
     scraper.scrape_page1()
-    #scraper.scrape_page2()
-    #scraper.scrape_page3()
+    scraper.scrape_page2()
+    scraper.scrape_page3()
     
 
 if __name__ == "__main__":
